885e9550ad4045af0c6f92574c2a3a157d91d00e
Alejo Boga <alejoboga20@gmail.com>
2023-04-14 11:04:54 -0400

    setup swr and error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import useSWR from 'swr';
+
 export const App = () => {
+	const { data, error } = useSWR('pokemon');
+
+	if (error) throw new Error();
+	if (data) return <div>{JSON.stringify(data)}</div>;
+
 	return <div>App</div>;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { SWRConfig } from 'swr';
+
 import { App } from './App';
+import { fetcher } from './api';
 import './index.css';
+import { ErrorBoundary } from 'react-error-boundary';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<SWRConfig
+			value={{
+				fetcher,
+				suspense: true,
+			}}
+		>
+			<Suspense fallback={<div>Dashboard skeleton</div>}>
+				<ErrorBoundary fallback={<div>Dashboard Broken</div>}>
+					<App />
+				</ErrorBoundary>
+			</Suspense>
+		</SWRConfig>
 	</React.StrictMode>
 );
