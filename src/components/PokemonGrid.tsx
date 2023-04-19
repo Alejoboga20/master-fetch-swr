26f447a3590d0cc6566bd86134ac799a08eb747f
Alejo Boga <alejoboga20@gmail.com>
2023-04-19 16:13:40 -0400

    button for pagination

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -1,18 +1,27 @@
-import { BasePokemon, PokemonResponse } from '../types/pokemon';
-import { PokemonCard } from './PokemonCard';
 import useSWR from 'swr';
+
+import { PokemonResponse } from '../types/pokemon';
+import { PokemonCard } from './PokemonCard';
 
 export const PokemonGrid = () => {
 	const { data } = useSWR<PokemonResponse>('pokemon?limit=20');
+
+	const onLoadMore = () => {};
 
 	if (!data) throw new Error();
 
 	return (
 		<div className='p-10'>
-			<div className='grid grid-cols-2 md:grid-cols-4 gap-4 w-full'>
+			<div className='grid grid-cols-2 md:grid-cols-4 gap-4 w-full mb-8'>
 				{data.results.map(({ name }, index) => (
 					<PokemonCard key={name} name={name} index={index + 1} />
 				))}
+			</div>
+
+			<div className='text-center'>
+				<button className='w-[500px] bg-slate-400 rounded p-2' onClick={onLoadMore}>
+					Load More
+				</button>
 			</div>
 		</div>
 	);
