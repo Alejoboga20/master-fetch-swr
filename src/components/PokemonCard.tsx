0f1c4441403a2ab6fcddf574e51f958a2135135e
Alejo Boga <alejoboga20@gmail.com>
2023-04-19 15:15:15 -0400

    fetch pokemon details

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,7 @@
 import { preload } from 'swr';
 import { fetcher } from '../api';
+import { PokemonDetails } from './PokemonDetails';
+import { Suspense, useState } from 'react';
 
 interface PokemonCardProps {
 	name: string;
@@ -7,8 +9,12 @@ interface PokemonCardProps {
 }
 
 export const PokemonCard = ({ name, index }: PokemonCardProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	const indexToShow =
 		index < 10 ? `00${index}` : index === 10 ? `0${index}` : index < 100 ? `0${index}` : index;
+
+	const onClick = () => setIsOpen(true);
 
 	const onHover = () => {
 		preload(`pokemon/${index}`, fetcher);
@@ -19,6 +25,7 @@ export const PokemonCard = ({ name, index }: PokemonCardProps) => {
 			<div
 				className='bg-slate-600 rounded-lg p-4 hover:cursor-pointer hover:bg-slate-950'
 				onMouseEnter={onHover}
+				onClick={onClick}
 			>
 				<img
 					className='w-full'
@@ -27,6 +34,12 @@ export const PokemonCard = ({ name, index }: PokemonCardProps) => {
 				/>
 				<div className='capitalize text-lg'>{name}</div>
 			</div>
+
+			{isOpen && (
+				<Suspense fallback={<div>Details skeleton</div>}>
+					<PokemonDetails isOpen={isOpen} onClose={() => setIsOpen(false)} index={index} />
+				</Suspense>
+			)}
 		</>
 	);
 };
--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,3 +1,31 @@
-export const PokemonDetails = () => {
-	return <div>PokemonDetails</div>;
+import { Suspense } from 'react';
+import closeModalIcon from '/icons/close-modal.svg';
+import useSWR from 'swr';
+
+interface ModalProps {
+	isOpen: boolean;
+	onClose: () => void;
+	index: number;
+}
+
+export const PokemonDetails = ({ isOpen, onClose, index }: ModalProps) => {
+	const { data } = useSWR(`pokemon/${index}`);
+
+	return (
+		<div className='absolute inset-0 z-10 h-screen'>
+			<div
+				className='flex h-full items-center justify-center bg-slate-400 bg-opacity-25'
+				onClick={(e) => {
+					if (e.target === e.currentTarget) onClose();
+				}}
+			>
+				<div className='relative w-[500px] h-[500px] rounded bg-black p-6'>
+					<button className='absolute right-6' onClick={onClose}>
+						<img alt='close modal' src={closeModalIcon} />
+					</button>
+					<div>{JSON.stringify(data)}</div>
+				</div>
+			</div>
+		</div>
+	);
 };
